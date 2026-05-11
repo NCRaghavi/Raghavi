0ba614a50d26d79c205e90b274691cfaa798c366
Raghavi <raghavichakravarthy@gmail.com>
2026-05-11 12:18:36 +0530

    Update testimonials.js

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -23,7 +23,7 @@ export const testimonialsData = {
       id: 101,
       name: "Dr. Pragnesh Dalwadi",
       topic: "Assistant Professor, Govt Arts and Commerce College, Gujarat",
-      image: "/images/testimonials/pragnesh.jpg",
+      image: "/images/testimonials/pragnesh.png",
       text: "Dr. Raghavi's mentorship during my Ph.D. was instrumental in shaping both my research perspective and academic discipline. Her constant encouragement and insightful guidance not only strengthened my work but also built the foundation of my professional journey.",
       stars: 5
     }
